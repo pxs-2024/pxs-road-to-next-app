432f027af7afbf7e6aedd6f4951b5452f14541c8
Pengshuo <pengxueshuo777@163.com>
2025-12-21 17:54:47 +0800

    feat: homepage and tickets 增加样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import { ticketsPath } from "@/path";
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1 className="text-lg"> Home Page !!!!</h1>
-			<Link href={ticketsPath()} className="underline">
-				Go to Tickets
-			</Link>
+		<div className="flex-1 flex flex-col gap-y-8">
+			<div>
+				<h1 className="text-3xl font-bold tracking-tight">Home Page</h1>
+				<p className="text-sm text-muted-foreground">Your home place to start</p>
+			</div>
+
+			<div className="flex-1 flex flex-col items-center">
+				<Link href={ticketsPath()} className="underline">
+					Go to Tickets
+				</Link>
+			</div>
 		</div>
 	);
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,17 +4,24 @@ import { initialTickets } from "../../data";
 
 const TicketsPage = () => {
 	return (
-		<div>
-			{initialTickets.map((ticket) => {
-				return (
-					<div className="text-lg" key={ticket.id}>
-						<h2>{ticket.title}</h2>
-						<Link href={ticketPath(ticket.id)} className="text-sm underline">
-							View
-						</Link>
-					</div>
-				);
-			})}
+		<div className="flex-1 flex flex-col gap-y-8">
+			<div>
+				<h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+				<p className="text-sm text-muted-foreground">All your tickets</p>
+			</div>
+			<div className="flex-1 flex flex-col items-center gap-y-4">
+				{initialTickets.map((ticket) => {
+					return (
+						<div key={ticket.id} className="w-full max-w-[420px] p-4 border border-slate-100 rounded">
+							<h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+							<p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+							<Link href={ticketPath(ticket.id)} className="text-sm underline">
+								View
+							</Link>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
