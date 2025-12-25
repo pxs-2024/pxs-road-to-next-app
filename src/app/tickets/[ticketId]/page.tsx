1bff0edad908ba7addd54c3e26cbbaa89eb11850
Pengshuo <pengxueshuo777@163.com>
2025-12-25 22:54:39 +0800

    feat: 了解服务端组件

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,20 +1,21 @@
-"use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 import { Placeholder } from "@/components/placeholder";
 import { Button } from "@/components/ui/button";
-import { initialTickets } from "@/data";
+import { getTicket } from "@/features/queries/get-ticket";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { ticketsPath } from "@/path";
 
-type TicketPageParams = {
-	ticketId: string;
+type TicketPageProps = {
+	params: {
+		ticketId: string;
+	};
 };
 
-const TicketPage = () => {
-	const params = useParams<TicketPageParams>();
+const TicketPage = async ({ params }: TicketPageProps) => {
+	const { ticketId } = await params;
+	const ticket = await getTicket(ticketId);
 
-	const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId);
+	// const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId);
 
 	if (!ticket) {
 		return (
@@ -31,7 +32,7 @@ const TicketPage = () => {
 
 	return (
 		<div className="flex justify-center animate-fade-in-from-top">
-			<TicketItem ticket={ticket} isDetail  ></TicketItem>
+			<TicketItem ticket={ticket} isDetail></TicketItem>
 		</div>
 	);
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,17 +1,15 @@
+import { Suspense } from "react";
 import { Heading } from "@/components/heading";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { initialTickets } from "../../data";
-
+import { Spinner } from "@/components/spinner";
+import { TicketList } from "@/features/ticket/components/ticket-list";
 
 const TicketsPage = () => {
 	return (
 		<div className="flex-1 flex flex-col gap-y-8">
 			<Heading title="TicketsPage" description="All your tickets" />
-			<div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-				{initialTickets.map((ticket) => {
-					return <TicketItem key={ticket.id} ticket={ticket} />;
-				})}
-			</div>
+			<Suspense fallback={<Spinner />}>
+				<TicketList />
+			</Suspense>
 		</div>
 	);
 };
