6908634a5292c456c298a6195fd1cdb2a6a93450
Pengshuo <pengxueshuo777@163.com>
2026-01-08 23:43:03 +0800

    feat: 修改prisma配置来修复线上问题

--- src/features/ticket/actions/update-ticket-status.ts
+++ src/features/ticket/actions/update-ticket-status.ts
@@ -1,12 +1,12 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { fromErrorToActionState, toActionState } from '@/components/form/utils/to-action-state';
 import { TicketStatus } from '@/generated/prisma/enums';
 import prisma from '@/lib/prisma';
 import { ticketsPath } from '@/path';
-import { revalidatePath } from 'next/cache';
 
-export const updateTicketStatus = async(id: string, status: TicketStatus) => {
+export const updateTicketStatus = async (id: string, status: TicketStatus) => {
   try {
     await prisma.ticket.update({
       where: { id },
