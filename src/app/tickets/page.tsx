99e5d8f25320059ee0279448a77a5ba273871246
Pengshuo <pengxueshuo777@163.com>
2026-01-12 21:56:47 +0800

    feat: ticket层增加layout校验用户权限

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,27 +1,28 @@
-import { CardCompact } from '@/components/card-compact';
-import { Heading } from '@/components/heading';
-import { Spinner } from '@/components/spinner';
-import { TicketList } from '@/features/ticket/components/ticket-list';
-import { TicketUpsertForm } from '@/features/ticket/components/ticket-upsert-form';
-import { Suspense } from 'react';
+import { CardCompact } from "@/components/card-compact";
+import { Heading } from "@/components/heading";
+import { Spinner } from "@/components/spinner";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 
-const TicketsPage = () => {
-  return (
-    <div className="flex-1 flex flex-col gap-y-8">
-      <Heading title="TicketsPage" description="All your tickets" />
+import { Suspense } from "react";
 
-      <CardCompact
-        title="Create Ticket"
-        description="A new Ticket will be created"
-        content={<TicketUpsertForm />}
-        className="w-full max-w-[420px] self-center"
-      />
+const TicketsPage = async () => {
+	return (
+		<div className="flex-1 flex flex-col gap-y-8">
+			<Heading title="TicketsPage" description="All your tickets" />
 
-      <Suspense fallback={<Spinner />}>
-        <TicketList />
-      </Suspense>
-    </div>
-  );
+			<CardCompact
+				title="Create Ticket"
+				description="A new Ticket will be created"
+				content={<TicketUpsertForm />}
+				className="w-full max-w-[420px] self-center"
+			/>
+
+			<Suspense fallback={<Spinner />}>
+				<TicketList />
+			</Suspense>
+		</div>
+	);
 };
 
 export default TicketsPage;
