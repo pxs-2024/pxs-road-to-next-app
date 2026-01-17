3986debe9ad6026e3c9bd532b0f402786db4ba01
Pengshuo <pengxueshuo777@163.com>
2026-01-17 17:24:47 +0800

    feat: api route

---
+++ src/app/api/tickets/[ticketId]/route.ts
@@ -0,0 +1,14 @@
+import { getTicket } from "@/features/ticket/queries/get-ticket";
+
+export async function GET(
+	_request: Request,
+	{
+		params,
+	}: {
+		params: { ticketId: string };
+	}
+) {
+	const ticket = await getTicket(params.ticketId);
+
+	return Response.json(ticket);
+}
